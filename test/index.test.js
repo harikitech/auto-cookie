29cc941a8af32e939d55c199488dfe0d31fb5065
taka-sho <takasho530000@gmail.com>
2017-03-28 18:28:52 +0900

    chore(test) fix lint

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,15 @@
 import assert from 'assert'
+import { describe, it } from 'mocha'
+import webdriverio from 'webdriverio'
 
 describe('auto-cookie', () => {
   const name = 'auto-cookie'
+  const config = {
+    desiredCapabilities: { browserName: 'chrome' }
+  }
+  const client = webdriverio.remote(config)
   it('set cookie', () => {
-    browser.url('http://www.0.0.0.0.xip.io:8000')
-    assert(browser.getCookie(name) === 'data')
+    client.url('http://www.0.0.0.0.xip.io:8000')
+    assert(client.getCookie(name) === 'data')
   })
 })
