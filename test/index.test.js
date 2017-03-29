f66ebf5df10995967c4cf32ae857140bcf157e09
taka-sho <takasho530000@gmail.com>
2017-03-29 13:44:31 +0900

    chore(travis) pass test on travis

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,12 @@
+/* @flow */
+/* global browser */
 import assert from 'assert'
 import { describe, it } from 'mocha'
-import webdriverio from 'webdriverio'
 
 describe('auto-cookie', () => {
   const name = 'auto-cookie'
-  const config = {
-    desiredCapabilities: { browserName: 'chrome' }
-  }
-  const client = webdriverio.remote(config)
   it('set cookie', () => {
-    client
+    browser
       .init()
       .url('http://www.0.0.0.0.xip.io:8000')
       .getCookie(name)
