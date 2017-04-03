97a7c16de3d334f307855b7d3a61d551dcc6d1a4
taka-sho <takasho530000@gmail.com>
2017-04-03 18:11:40 +0900

    chore(test) add locahost test

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,32 @@
 /* @flow */
 import assert from 'assert'
-import { describe, it } from 'mocha'
+import { describe, beforeEach, it } from 'mocha'
 
 declare var browser: any
 
 describe('auto-cookie', () => {
   const name = 'auto-cookie'
-  it('set cookie', (done) => {
+
+  beforeEach(() => {
+    browser.deleteCookie()
+  })
+
+  it('should get cookie around xip.io', (done) => {
     browser
       .url('http://www.0.0.0.0.xip.io:8000')
       .getCookie(name)
       .then((cookie) => {
         assert.equal(cookie.value, 'data')
+        done()
+      })
+  })
+
+  it('should not set cookie around localhost', (done) => {
+    browser
+      .url('http://localhost:8000')
+      .getCookie(name)
+      .then((cookie) => {
+        assert(!cookie.value)
         done()
       })
   })
