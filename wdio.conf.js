fc89661e3ed024f6f1923021f936728f70bc8913
Keisuke Kan <kan@uncovertruth.co.jp>
2017-04-06 11:13:34 +0900

    feat(package) add sauce options

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,4 @@
-exports.config = {
+const config = {
   specs: [
     './test/*.js'
   ],
@@ -12,7 +12,6 @@ exports.config = {
   sync: true,
   logLevel: 'silent',
   coloredLogs: true,
-  baseUrl: 'http://localhost',
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
@@ -24,3 +23,25 @@ exports.config = {
     compilers: ['js:babel-register']
   }
 }
+
+let override = {}
+if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY && process.env.CI_MODE === 'sauce') {
+  override = {
+    capabilities: [
+      {browserName: 'iphone', version: '10.0'},
+      {browserName: 'android', version: '5.1'},
+      {browserName: 'chrome', platform: 'Linux', version: '46'},
+      {browserName: 'chrome', version: 'beta'},
+      {browserName: 'firefox', platform: 'Linux', version: '44'},
+      {browserName: 'safari', version: '10.0'},
+      {browserName: 'internet explorer', platform: 'Windows 8.1', version: '11'},
+      {browserName: 'MicrosoftEdge', platform: 'Windows 10', version: '13.10586'}
+    ],
+    services: ['sauce'],
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    sauceConnect: false
+  }
+}
+
+exports.config = Object.assign({}, config, override)
