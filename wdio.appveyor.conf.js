f6cb67ef209f0fa0f670bbfc026881e0f2650300
taka-sho <takasho530000@gmail.com>
2017-04-07 17:02:40 +0900

    chore(wdioconf) add wdio.conf for appveyor

---
+++ wdio.appveyor.conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+  specs: [
+    './tmp/*.js'
+  ],
+  capabilities: [
+    {
+      browserName: 'IE'
+    }
+  ],
+  exclude: [],
+  bail: 0,
+  sync: true,
+  logLevel: 'verbose',
+  coloredLogs: true,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  framework: 'mocha',
+  reporters: ['dot', 'spec'],
+  services: ['selenium-standalone'],
+  mochaOpts: {
+    ui: 'bdd',
+    compilers: ['js:babel-register']
+  }
+}
