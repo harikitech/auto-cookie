026bf6021be9205172d9af151df25e9fc01efe2a
Shuta Kumano <shoota.dev@gmail.com>
2017-09-22 16:33:49 +0900

    fix test option interface

--- example/index.js
+++ example/index.js
@@ -1,13 +1,15 @@
 /* @flow */
 import { save, find } from '../src'
 const expires = 1 / 48
+const path='/path'
+const secure=true
 
 const name_basic = 'auto-cookie'
-save(name_basic, 'data', expires)
+save(name_basic, 'data', {expires})
 
 const name_path = 'path-cookie'
-save(name_path, 'setPath', expires, '/path')
+save(name_path, 'setPath', {expires, path})
 
 const name_secure = 'secure-cookie'
-save(name_secure, 'setPath', expires, '/path', true)
+save(name_secure, 'setPath', {expires, path, secure})
 console.log(find(name, expires))
