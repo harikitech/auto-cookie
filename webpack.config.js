b92c8fb63f5907adfc227b1f985dfa89631e3f2e
Shuta Kumano <shoota.dev@gmail.com>
2017-09-25 13:25:35 +0900

    typescript build by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,19 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    'app': path.join(__dirname, 'example/index.js')
+    app: path.join(__dirname, 'example/index.js')
   },
   output: {
     path: path.join(__dirname, 'example/'),
     filename: '[name].bundle.js'
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
   }
 }
