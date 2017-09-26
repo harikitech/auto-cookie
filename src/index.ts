291eedd95ca7ccd355f099aee88a9f88b019d6c5
Shuta Kumano <shoota.dev@gmail.com>
2017-09-26 17:50:14 +0900

    refactor function

--- src/index.ts
+++ src/index.ts
@@ -5,41 +5,42 @@ function removeNaked (): string {
   return domain.indexOf('www.') === 0 ? domain.substring(4) : domain
 }
 
+function setCookie (
+  domainParts: Array<string>,
+  name: string,
+  options: cookies.CookieAttributes,
+  data: string
+): string | undefined {
+  let domain = domainParts[domainParts.length - 1]
+  let attr: cookies.CookieAttributes = options
+  for (let i = 2; i <= domainParts.length; i++) {
+    domain = `${domainParts[domainParts.length - i]}.${domain}`
+    attr = Object.assign({}, options, { domain })
+    cookies.set(name, data, options)
+    const storedId = cookies.get(name)
+    if (storedId) {
+      return storedId
+    }
+  }
+  cookies.set(name, data, attr)
+  return cookies.get(name)
+}
+
 function findOrCreate (
   name: string,
   options: cookies.CookieAttributes,
   data?: string
 ): string | undefined {
-  const value = cookies.get(name)
-  if (value) {
-    return value
-  }
-
-  const domainParts = removeNaked().split('.')
+  const domainParts: Array<string> = removeNaked().split('.')
   const subDomain: any = domainParts[domainParts.length - 1]
   /* tslint:disable:triple-equals */
   if (domainParts.length === 4 && parseInt(subDomain, 10) == subDomain) {
     return cookies.get(name)
   }
 
-  let domain = domainParts[domainParts.length - 1]
-  let attr: cookies.CookieAttributes = options
-
-  for (let i = 2; i <= domainParts.length; i++) {
-    domain = `${domainParts[domainParts.length - i]}.${domain}`
-    attr = Object.assign({}, options, { domain })
-
-    if (data) {
-      cookies.set(name, data, attr)
-    }
-
-    const storedId = cookies.get(name)
-    if (storedId) {
-      return storedId
-    }
-  }
+  let created
   if (data) {
-    cookies.set(name, data, attr)
+    created = setCookie(domainParts, name, options, data)
   }
   return cookies.get(name)
 }
@@ -48,6 +49,10 @@ export function find (
   name: string,
   options: cookies.CookieAttributes
 ): string | undefined {
+  const value = cookies.get(name)
+  if (value) {
+    return value
+  }
   return findOrCreate(name, options)
 }
 
