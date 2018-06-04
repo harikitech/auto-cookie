66d35ae79d95799fdc83316f5d3aadbb6817cd1a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-04 08:37:10 +0000

    Deploy uncovertruth/auto-cookie to github.com/uncovertruth/auto-cookie.git:gh-pages

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,3 +1,2 @@
 import * as cookies from 'js-cookie';
-export declare function find(name: string): string | undefined;
-export declare function save(name: string, value: string | object, options: cookies.CookieAttributes): string | undefined;
+export declare function save(name: string, data?: string | object, options?: cookies.CookieAttributes): string | undefined;
