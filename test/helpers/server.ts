501d155fe026b105f9c717d5804e6c3cda31fe55
Shoota Kumano <shoota.dev@gmail.com>
2018-06-26 11:29:10 +0900

    Fix/espower typescript (#297)
    * chore(deps): update dependency espower-typescript to v9
    
    * upgrade lock
    
    * update types/webdriverio
    
    * fix typing

--- test/helpers/server.ts
+++ test/helpers/server.ts
@@ -3,7 +3,7 @@ import * as http from 'http'
 import * as path from 'path'
 import * as serveStatic from 'serve-static'
 
-export default function start (callback?: () => void) {
+export default function start (callback?: () => void): http.Server {
   const serve = serveStatic(path.join(__dirname, '../../example'))
   const server = http.createServer((req: any, res: any) => {
     serve(req, res, finalhandler(req, res))
