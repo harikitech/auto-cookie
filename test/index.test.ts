ed9fdba74b5dd4831d3e64f1bb1d2bc6e485dcfc
Keisuke Kan <9renpoto@gmail.com>
2020-04-01 21:17:38 +0900

    chore: Added semicolon

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,47 +1,47 @@
-import * as assert from 'assert'
-import * as http from 'http'
-import 'mocha'
-import 'webdriverio'
-import start from './helpers/server'
+import * as assert from "assert";
+import * as http from "http";
+import "mocha";
+import "webdriverio";
+import start from "./helpers/server";
 
-describe('auto-cookie', () => {
-  let server: http.Server
-  const COOKIE_NAME = 'auto-cookie'
+describe("auto-cookie", () => {
+  let server: http.Server;
+  const COOKIE_NAME = "auto-cookie";
 
   before((done) => {
-    server = start(done)
-  })
+    server = start(done);
+  });
 
-  after(() => server.close())
-  beforeEach('clean cookies', () => browser.deleteAllCookies())
+  after(() => server.close());
+  beforeEach("clean cookies", () => browser.deleteAllCookies());
 
-  it('should get cookie around xip.io', async () => {
-    await browser.url('http://www.0.0.0.0.xip.io:8000')
-    const [cookie] = await browser.getCookies([COOKIE_NAME])
-    assert(cookie.value === 'data')
-    assert(cookie.domain === '.xip.io')
-  })
+  it("should get cookie around xip.io", async () => {
+    await browser.url("http://www.0.0.0.0.xip.io:8000");
+    const [cookie] = await browser.getCookies([COOKIE_NAME]);
+    assert(cookie.value === "data");
+    assert(cookie.domain === ".xip.io");
+  });
 
-  it('should not set cookie around 0.0.0.0', async () => {
-    await browser.url('http://0.0.0.0:8000')
-    const [cookie] = await browser.getCookies([COOKIE_NAME])
-    assert(!cookie)
-  })
+  it("should not set cookie around 0.0.0.0", async () => {
+    await browser.url("http://0.0.0.0:8000");
+    const [cookie] = await browser.getCookies([COOKIE_NAME]);
+    assert(!cookie);
+  });
 
-  it('should get cookie with path', async () => {
-    await browser.url('http://www.0.0.0.0.xip.io:8000/path')
-    const [cookie] = await browser.getCookies(['path-cookie'])
-    assert(cookie.value === 'setPath')
-    assert(cookie.domain === '.0.0.0.0.xip.io')
-    assert(cookie.path === '/path')
-  })
+  it("should get cookie with path", async () => {
+    await browser.url("http://www.0.0.0.0.xip.io:8000/path");
+    const [cookie] = await browser.getCookies(["path-cookie"]);
+    assert(cookie.value === "setPath");
+    assert(cookie.domain === ".0.0.0.0.xip.io");
+    assert(cookie.path === "/path");
+  });
 
-  it('should get cookie has object value', async () => {
-    await browser.url('http://www.0.0.0.0.xip.io:8000')
-    const [cookie] = await browser.getCookies(['object-cookie'])
+  it("should get cookie has object value", async () => {
+    await browser.url("http://www.0.0.0.0.xip.io:8000");
+    const [cookie] = await browser.getCookies(["object-cookie"]);
     assert(
-      cookie.value === '{%22s%22:%22string%22%2C%22n%22:99%2C%22b%22:true}',
-    )
-    assert(cookie.domain === '.xip.io')
-  })
-})
+      cookie.value === "{%22s%22:%22string%22%2C%22n%22:99%2C%22b%22:true}"
+    );
+    assert(cookie.domain === ".xip.io");
+  });
+});
