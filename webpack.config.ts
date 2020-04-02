4fd248a80ee24347f3f149db6de59eb1febf09c0
Keisuke Kan <9renpoto@gmail.com>
2020-04-02 09:25:03 +0900

    chore: use size-plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,19 @@
-const path = require('path')
+const path = require("path");
+const SizePlugin = require("size-plugin");
 
 module.exports = {
   entry: {
-    app: path.join(__dirname, 'example/index.ts')
+    app: path.join(__dirname, "example/index.ts"),
   },
   output: {
-    path: path.join(__dirname, 'example/'),
-    filename: '[name].bundle.js'
+    path: path.join(__dirname, "example/"),
+    filename: "[name].bundle.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
-    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
-  }
-}
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+  plugins: [new SizePlugin({ publish: true })],
+};
