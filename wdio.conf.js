d651d269b4e6d8aab09083836f80f53b68480019
Keisuke Kan <9renpoto@gmail.com>
2020-05-08 12:24:43 +0900

    refactor: revert codestyle

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,31 +1,31 @@
 /** @type import('@wdio/sync').Config */
 exports.config = {
-  runner: "local",
-  specs: ["./test/*.ts"],
+  runner: 'local',
+  specs: ['./test/*.ts'],
   exclude: [],
   maxInstances: 10,
   capabilities: [
     {
       maxInstances: 5,
-      browserName: "chrome",
-      "goog:chromeOptions": {
-        args: ["--headless", "--disable-gpu"],
+      browserName: 'chrome',
+      'goog:chromeOptions': {
+        args: ['--headless', '--disable-gpu'],
       },
     },
   ],
-  logLevel: "info",
+  logLevel: 'info',
   bail: 0,
-  baseUrl: "http://localhost",
+  baseUrl: 'http://localhost',
   waitforTimeout: 15000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
-  services: ["chromedriver"],
-  framework: "mocha",
-  reporters: ["spec"],
+  services: ['chromedriver'],
+  framework: 'mocha',
+  reporters: ['spec'],
   mochaOpts: {
-    ui: "bdd",
-    require: ["espower-typescript/guess"],
+    ui: 'bdd',
+    require: ['espower-typescript/guess'],
     timeout: 100000,
   },
   port: 9515,
-};
+}
